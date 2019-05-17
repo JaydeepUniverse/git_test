ecfef877cf70149fcafe7ee7f31cb25acb46e2a5
JaydeepUniverse <ecjaydeepsoni@gmail.com>
2019-05-17 08:39:04 +0530

    create file_write.py

---
+++ file_write.py
@@ -0,0 +1,9 @@
+target3 = open("test.txt",'w')
+line1 = "Hello"
+line2 = "How Are You"
+target3.write(line1)
+target3.write("\n")
+target3.write(line2)
+target3.write("\n")
+target3.close()
+print ("Hello")
